c8c67aa993b18fe44765a96aa15e48ac61ee2842
rafael-calixto1 <77207863+rafael-calixto1@users.noreply.github.com>
2022-03-21 22:41:46 -0300

    incomplet execise of a bank to learn encapsulatio

---
+++ vendas/Encapsulamento/Encapsulamento/Conta.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulamento
+{
+    public class Conta
+    {
+        public string _titular;
+        private double _saldo;
+        public int _agencia;
+
+
+        //Metodos Construtores
+
+        public Conta()
+        {
+        }
+
+        public Conta(string titular, double saldo, int agencia)
+        {
+            _titular = titular;
+            _saldo = saldo;
+            _agencia = agencia;
+
+        }
+
+
+
+        //ENCAPSULAMENTO é um metodo que permite a segurança
+        //GET - pegar / SET - colocar
+
+        public double GetSaldo()
+        {
+            return _saldo;
+        }
+
+        public void SetDeposito(double valor)
+        {
+            _saldo += valor;
+        }
+            
+
+
+    }
+}
---
+++ vendas/Encapsulamento/Encapsulamento/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulamento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conta conta = new Conta();
+            Console.WriteLine("Digite o nome do titular");
+            conta._titular = Console.ReadLine();
+            Console.WriteLine("Digite o numero da agencia");
+            conta._agencia = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o valor do primeiro deposito");
+            double deposito = double.Parse(Console.ReadLine());
+            conta.SetDeposito(deposito);
+
+            //conta.GetSaldo() = 1000000;
+            Console.WriteLine("\n" +
+                $"Saldo: {conta.GetSaldo().ToString("C2")}");
+        }
+    }
+}
